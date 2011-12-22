562bf968e590f0b2bc29f6842e96fa2981460372
Matthew Schlau <mrschlau@gmail.com>
2011-12-22 15:46:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiblioTrial1::Application.config.secret_token = '240159f1b163d7ea99d31ecc80d662bd8e8a64ad374bc9fb0edd1ed1c5860dea3fe09eb91c4441186836958a86ea21286624df75066c402ce80b42a1030f10f7'
